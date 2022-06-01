5bbde51174f338aa7fb0bec6abed78edb6b42540
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2022-06-01 21:43:25 +0600

    stack done

---
+++ link-list.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+
+    struct Node
+{
+    int Data;
+    struct Node *Next;
+};
+
+{
+    return 0;
+}
--- tempCodeRunnerFile.c
+++
@@ -1,5 +0,0 @@
- for (int i = 0; i < sizeof(a) / sizeof(int); i++)
-    {
-        printf("%u\n", &a[i]);
-    }
-    printf("\n\n");
