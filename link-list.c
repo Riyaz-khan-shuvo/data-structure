898781f37afc865583835b57f0a44d1b6c7536b6
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2022-06-02 10:42:48 +0600

    link list done

--- link-list.c
+++ link-list.c
@@ -1,12 +1,39 @@
 #include <stdio.h>
-int main()
-
-    struct Node
+#include <stdlib.h>
+struct Node
 {
     int Data;
     struct Node *Next;
 };
 
+int main()
 {
+    struct Node *a = NULL;
+    struct Node *b = NULL;
+    struct Node *c = NULL;
+
+    a = (struct Node *)malloc(sizeof(struct Node));
+    b = (struct Node *)malloc(sizeof(struct Node));
+    c = (struct Node *)malloc(sizeof(struct Node));
+
+    a->Data = 10;
+    b->Data = 20;
+    c->Data = 30;
+
+    a->Next = b;
+    b->Next = c;
+    c->Next = NULL;
+
+// traversing the link list
+
+    while (a != NULL)
+    {
+        printf("%d ", a->Data);
+        a = a->Next;
+    }
+    printf("\n %p" , &a);
+    printf("\n %p" , &b);
+    printf("\n %p" , &c);
+
     return 0;
 }
